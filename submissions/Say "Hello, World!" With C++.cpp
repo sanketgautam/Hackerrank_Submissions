228c92b018b2878392741ceb9866af93a32973f6
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-05-18 11:10:17 +0530

    added Say "Hello, World!" With C++

---
+++ submissions/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Say "Hello, World!" With C++
+Problem Link: https://www.hackerrank.com/challenges/cpp-hello-world
+Author: sanketgautam
+Language : C++14
+
+-----------------------------------------------------------------------*/
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
