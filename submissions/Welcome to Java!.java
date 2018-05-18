3228ce40487c035bc03de2462a1a89e403d2b7d0
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-05-18 11:11:18 +0530

    added Welcome to Java!

---
+++ submissions/Welcome to Java!.java
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Welcome to Java!
+Problem Link: https://www.hackerrank.com/challenges/welcome-to-java
+Author: sanketgautam
+Language : Java 7
+
+-----------------------------------------------------------------------*/
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
