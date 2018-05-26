0288b1e3e1249a93ebbb68190501470895e2e33b
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-05-26 12:53:13 +0530

    added Structs

---
+++ submissions/Structs.cpp
@@ -0,0 +1,33 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Structs
+Problem Link: https://www.hackerrank.com/challenges/c-tutorial-struct
+Author: sanketgautam
+Language : C++14
+
+-----------------------------------------------------------------------*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student {
+    string first_name;
+    string last_name;
+    int standard;
+    int age;
+};
+
+int main()
+{
+    Student st;
+
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+
+    return 0;
+}
