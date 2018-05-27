cfefdfd7581f5b7ff1586ec62b23abd97dd5c96d
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-05-27 11:32:03 +0530

    added Inheritance Introduction

---
+++ submissions/Inheritance Introduction.cpp
@@ -0,0 +1,49 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Inheritance Introduction
+Problem Link: https://www.hackerrank.com/challenges/inheritance-introduction
+Author: sanketgautam
+Language : C++14
+
+-----------------------------------------------------------------------*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Triangle {
+public:
+    void triangle()
+    {
+
+        cout << "I am a triangle\n";
+    }
+};
+
+class Isosceles : public Triangle {
+public:
+    void isosceles()
+    {
+
+        cout << "I am an isosceles triangle\n";
+    }
+
+    void description()
+    {
+        cout << "In an isosceles triangle two sides are equal\n";
+    }
+};
+
+int main()
+{
+    Isosceles isc;
+    isc.isosceles();
+    isc.description();
+
+    isc.triangle();
+    return 0;
+}
