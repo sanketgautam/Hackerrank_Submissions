2263a03eeb0107cd6557fdd3211e3e8545d9291a
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-05-27 11:31:27 +0530

    added Hotel Prices

---
+++ submissions/Hotel Prices.cpp
@@ -0,0 +1,10 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Hotel Prices
+Problem Link: https://www.hackerrank.com/challenges/hotel-prices
+Author: sanketgautam
+Language : C++14
+
+-----------------------------------------------------------------------*/
+
+
