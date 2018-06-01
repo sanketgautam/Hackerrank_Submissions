2de4d3ae5d0e713efeb386f4554dc369cf31194f
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-06-01 23:10:43 +0530

    added Insert a node at a specific position in a linked list

---
+++ submissions/Insert a node at a specific position in a linked list.cpp
@@ -0,0 +1,10 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Insert a node at a specific position in a linked list
+Problem Link: https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list
+Author: sanketgautam
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
