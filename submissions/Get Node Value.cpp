810c74cffe1283647d988d68fd1cd7a01c61a815
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-06-01 23:10:21 +0530

    added Get Node Value

---
+++ submissions/Get Node Value.cpp
@@ -0,0 +1,10 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Get Node Value
+Problem Link: https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail
+Author: sanketgautam
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
