c20fb0035bab938b7e09917f660e4573565805e9
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-06-01 23:11:12 +0530

    added Dynamic Array

---
+++ submissions/Dynamic Array.cpp
@@ -0,0 +1,10 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Dynamic Array
+Problem Link: https://www.hackerrank.com/challenges/dynamic-array
+Author: sanketgautam
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
