0087830f6e515a0e3e91f62c27e9b04dfe36096b
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-06-01 23:10:25 +0530

    added Merge two sorted linked lists

---
+++ submissions/Merge two sorted linked lists.cpp
@@ -0,0 +1,10 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Merge two sorted linked lists
+Problem Link: https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists
+Author: sanketgautam
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
