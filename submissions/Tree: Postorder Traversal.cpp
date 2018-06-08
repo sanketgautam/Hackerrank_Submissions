772c7efabe9a98eae8a0082e5607982166635a1e
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-06-08 22:49:59 +0530

    updated Tree: Postorder Traversal

--- submissions/Tree: Postorder Traversal.cpp
+++ submissions/Tree: Postorder Traversal.cpp
@@ -22,9 +22,58 @@ struct node
 
 void postOrder(node* root)
 {
+
+    node *cur, *prev, *first, *middle, *last, *dummy;
+
     if (!root)
         return;
-    postOrder(root->left);
-    postOrder(root->right);
-    cout << root->data << " ";
+
+    dummy = new node();
+    dummy->left = root;
+    dummy->right = NULL;
+
+    cur = dummy;
+
+    while (cur) {
+        if (!cur->left) {
+            cur = cur->right;
+        }
+        else {
+            prev = cur->left;
+
+            while (prev->right != NULL && prev->right != cur) {
+                prev = prev->right;
+            }
+
+            if (!prev->right) {
+                prev->right = cur;
+                cur = cur->left;
+            }
+            else {
+                //reverse the right references in chain from cur to prev
+                first = cur;
+                middle = cur->left;
+                while (middle != cur) {
+                    last = middle->right;
+                    middle->right = first;
+                    first = middle;
+                    middle = last;
+                }
+
+                //again reverse the right references from prev to cur & print cur->data
+                first = cur;
+                middle = prev;
+                while (middle != cur) {
+                    cout << middle->data << " ";
+                    last = middle->right;
+                    middle->right = first;
+                    first = middle;
+                    middle = last;
+                }
+
+                prev->right = NULL;
+                cur = cur->right;
+            }
+        }
+    }
 }
