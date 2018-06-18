4c1bcd5e866827420484de05d2afabca0931947c
Sanket Kumar Gautam <sk2gautam@gmail.com>
2018-06-18 17:35:57 +0530

    completed missing code

--- submissions/Queue using Two Stacks.cpp
+++ submissions/Queue using Two Stacks.cpp
@@ -16,21 +16,34 @@ Language : C++
 #include <stack>
 using namespace std;
 
-int main()
-{
+
+int main() {
     int q, t, n;
     stack<int> s1, s2;
-    bool flag = true;
-    cin >> q;
-
-    while (q--) {
-
-        if (t == 1) {
-            cin >> n;
+    bool flag=true;
+    cin>>q;
+    
+    while(q--){
+        cin>>t;
+        if(t==1){
+            cin>>n;
             s1.push(n);
-        }
-        else if ((t == 2) || (t == 3)) { //it will take O(n)
-
-            if (s2.empty()) {
-                while (!s1.empty()) {
+        }else if((t==2)||(t==3)){ //it will take O(n)
+            
+            if(s2.empty()){
+                while(!s1.empty()){
                     n = s1.top();
+                    s2.push(n);
+                    s1.pop();
+                }
+            }
+            
+            if(t==2)
+                s2.pop();
+            else
+                cout<<s2.top()<<"\n";
+        }
+    }
+    
+    return 0;
+}
